ac2cb2969aa364d34667868d1dd00b711ffbb74e
Amir Abaris <thisisamirabaris@gmail.com>
2025-09-17 11:48:49 +0330

    feat: removed extra boilderplate codes

---
+++ mess/Views/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  mess
+//
+//  Created by Amir Abaris on 9/16/25.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ContentView: View {
+    var body: some View {
+        HStack {
+            Button("button") {
+                // action
+            }
+            .buttonStyle(.glass)
+            
+        }
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
